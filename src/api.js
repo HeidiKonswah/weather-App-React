fcb3e48ceb24c265df3444257e1b9e03611c4f84
Heidi Konswah <heidikonswah@emushrifs-MacBook-Pro.local>
2023-01-19 00:30:46 +0200

    initial commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SKAxct8ZM7mshjKCMXC6S3j5hh1Zp1y8j2OjsnJ6RWoN0CwqXP',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; 
